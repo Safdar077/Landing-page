bc75176151f6d03b8121dd3808d4894deaf7e858
Safdar077 <sk8005543@gmail.com>
2025-11-13 22:15:40 +0530

    Removed about page

--- src/app/about/page.jsx
+++
@@ -1,5 +0,0 @@
-export default function about(){
-    return <div className="flex ml-30 items-center h-screen m-auto">
-        <div className="text-4xl font-bold">This is an about page</div>
-    </div>
-}
